65ba4e7810ed6cde25aec31c13819c0215361ca5
gaosiqiang <gaosiqiang7@163.com>
2022-05-30 03:34:25 +0800

    +)add file:TuringmachineController

--- app/controller/TuringmachineController.php
+++ app/controller/TuringmachineController.php
@@ -7,12 +7,66 @@ namespace app\controller;
 class TuringmachineController extends CommonController
 {
 
-    public function actionAddInstructions()
+    /**
+     * 图灵机主页
+     */
+    public function actionHome()
     {
 
     }
 
-    public function actionCurrent()
+    /**
+     * 添加程序
+     */
+    public function actionAddProgram()
+    {
+
+    }
+
+    /**
+     * 修改程序
+     */
+    public function actionSetProgram()
+    {
+
+    }
+
+    /**
+     * 获取当前程序执行行号
+     */
+    public function actionCurrentLine()
+    {
+
+    }
+
+    /**
+     * 获取数据带
+     */
+    public function actionDataTape()
+    {
+
+    }
+
+    /**
+     * 获取程序
+     */
+    public function actionGetProgram()
+    {
+
+    }
+
+    /**
+     * 执行一下个指令
+     */
+    public function actionExecuteNext()
+    {
+
+    }
+
+    /**
+     * 执行指定指令
+     */
+    public function actionExecuteLine()
     {
 
     }
